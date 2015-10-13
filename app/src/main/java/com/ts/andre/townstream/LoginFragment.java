9c6467b02ddab9414d9d93d6391ca90bcec916da
Andre Santos <msantos.aaugusto@gmail.com>
2015-10-13 01:32:21 -0300

    Atualizado, criado atividade para Login enviar nome do usuário logado.

--- app/src/main/java/com/ts/andre/townstream/LoginFragment.java
+++ app/src/main/java/com/ts/andre/townstream/LoginFragment.java
@@ -1,11 +1,8 @@
 package com.ts.andre.townstream;
 
-import android.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -15,17 +12,10 @@ import com.facebook.AccessTokenTracker;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
-import com.facebook.FacebookGraphResponseException;
-import com.facebook.FacebookSdk;
-import com.facebook.GraphRequest;
-import com.facebook.GraphResponse;
 import com.facebook.Profile;
 import com.facebook.ProfileTracker;
-import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
-
-import org.json.JSONObject;
 
 public class LoginFragment extends android.support.v4.app.Fragment {
     CallbackManager callbackManager;
@@ -34,14 +24,12 @@ public class LoginFragment extends android.support.v4.app.Fragment {
     AccessToken accessToken;
     ProfileTracker profileTracker;
     Profile userProfile;
+    public final static String EXTRA_NAME = "com.ts.andre.townstream.NAME";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                 Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.activity_login_fragment, container, false);
-
-
-
         info = (TextView)view.findViewById(R.id.info);
 
         callbackManager = CallbackManager.Factory.create();
@@ -59,7 +47,6 @@ public class LoginFragment extends android.support.v4.app.Fragment {
         accessToken = AccessToken.getCurrentAccessToken();
 
 
-
         profileTracker = new ProfileTracker() {
             @Override
             protected void onCurrentProfileChanged(Profile oldProfile, Profile currentProfile) {
@@ -71,13 +58,7 @@ public class LoginFragment extends android.support.v4.app.Fragment {
         userProfile = Profile.getCurrentProfile();
 
         if(accessToken!=null){
-            info.setText(
-                    "User name: "
-                            +userProfile.getFirstName()
-                            +"\n"+
-                            "Auth Token: "
-                            +accessToken.getToken()
-            );
+            sendUserInfo(userProfile);
         }
 
 
@@ -92,13 +73,7 @@ public class LoginFragment extends android.support.v4.app.Fragment {
             public void onSuccess(LoginResult loginResult) {
 
 
-                info.setText(
-                        "User Name: "
-                        +userProfile.getFirstName()
-                        +"\n"+
-                                "Auth Token: "
-                        +loginResult.getAccessToken().getToken()
-                );
+                sendUserInfo(userProfile);
 
                 }
 
@@ -130,5 +105,12 @@ public class LoginFragment extends android.support.v4.app.Fragment {
         super.onDestroy();
         accessTokenTracker.stopTracking();
         profileTracker.stopTracking();
+    }
+
+    public void sendUserInfo(Profile profile){
+        Intent intent = new Intent(getActivity(), UserInfo.class);
+        String name = profile.getName();
+        intent.putExtra(EXTRA_NAME, name);
+        startActivity(intent);
     }
 }
